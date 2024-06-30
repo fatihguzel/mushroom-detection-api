4527dd7cb3f2f9302df5af2ca94b4a527cfe8f4a
FATİH GÜZEL <98105601+fatihguzel@users.noreply.github.com>
2024-06-30 18:17:49 +0300

    refactor: Update model file name in app.py

--- app.py
+++ app.py
@@ -14,36 +14,34 @@ import openai
 
 app = Flask(__name__)
 
-model = load_model("google_model.h5")
+model = load_model("google_model_90.h5")
 
 # Class indices dictionary
 class_indices_mobile = {
-    'Amanita citrina - Yenilemez': 0, 'Amanita muscaria - Yenilemez': 1, 'Amanita pantherina - Yenilemez': 2, 'Amanita rubescens - Yenilebilir': 3,
-    'Apioperdon pyriforme - Yenilebilir': 4, 'Armillaria borealis - Yenilebilir': 5, 'Artomyces pyxidatus - Yenilebilir': 6, 'Bjerkandera adusta - Yenilemez': 7,
-    'Boletus edulis - Yenilebilir': 8, 'Boletus reticulatus - Yenilebilir': 9, 'Calocera viscosa - Yenilemez': 10, 'Calycina citrina - Yenilemez': 11,
-    'Cantharellus cibarius - Yenilebilir': 12, 'Cerioporus squamosus - Yenilebilir': 13, 'Cetraria islandica - Yenilebilir': 14, 'Chlorociboria aeruginascens - Yenilemez': 15,
-    'Chondrostereum purpureum - Yenilemez': 16, 'Cladonia fimbriata - Yenilemez': 17, 'Cladonia rangiferina - Yenilebilir': 18, 'Cladonia stellaris - Yenilebilir': 19,
-    'Clitocybe nebularis - Yenilemez': 20, 'Coltricia perennis - Yenilemez': 21, 'Coprinellus disseminatus - Yenilebilir': 22, 'Coprinellus micaceus - Yenilebilir': 23,
-    'Coprinopsis atramentaria - Yenilemez': 24, 'Coprinus comatus - Yenilebilir': 25, 'Crucibulum laeve - Yenilemez': 26, 'Daedaleopsis confragosa - Yenilemez': 27,
-    'Daedaleopsis tricolor - Yenilemez': 28, 'Evernia mesomorpha - Yenilemez': 29, 'Evernia prunastri - Yenilemez': 30, 'Flammulina velutipes - Yenilebilir': 31,
-    'Fomes fomentarius - Yenilemez': 32, 'Fomitopsis betulina - Yenilemez': 33, 'Fomitopsis pinicola - Yenilemez': 34, 'Ganoderma applanatum - Yenilemez': 35,
-    'Graphis scripta - Yenilemez': 36, 'Gyromitra esculenta - Yenilemez': 37, 'Gyromitra gigas - Yenilemez': 38, 'Gyromitra infula - Yenilemez': 39,
-    'Hericium coralloides - Yenilebilir': 40, 'Hygrophoropsis aurantiaca - Yenilemez': 41, 'Hypholoma fasciculare - Yenilemez': 42, 'Hypholoma lateritium - Yenilebilir': 43,
-    'Hypogymnia physodes - Yenilemez': 44, 'Imleria badia - Yenilebilir': 45, 'Inonotus obliquus - Yenilebilir': 46, 'Kuehneromyces mutabilis - Yenilebilir': 47,
-    'Lactarius deliciosus - Yenilebilir': 48, 'Lactarius torminosus - Yenilemez': 49, 'Lactarius turpis - Yenilemez': 50, 'Laetiporus sulphureus - Yenilebilir': 51,
-    'Leccinum albostipitatum - Yenilebilir': 52, 'Leccinum aurantiacum - Yenilebilir': 53, 'Leccinum scabrum - Yenilebilir': 54, 'Leccinum versipelle - Yenilebilir': 55,
-    'Lepista nuda - Yenilebilir': 56, 'Lobaria pulmonaria - Yenilebilir': 57, 'Lycoperdon perlatum - Yenilebilir': 58, 'Macrolepiota procera - Yenilebilir': 59,
-    'Merulius tremellosus - Yenilemez': 60, 'Mutinus ravenelii - Yenilemez': 61, 'Nectria cinnabarina - Yenilemez': 62, 'Panellus stipticus - Yenilemez': 63,
-    'Parmelia sulcata - Yenilemez': 64, 'Paxillus involutus - Yenilemez': 65, 'Peltigera aphthosa - Yenilebilir': 66, 'Peltigera praetextata - Yenilemez': 67,
-    'Phaeophyscia orbicularis - Yenilemez': 68, 'Phallus impudicus - Yenilemez': 69, 'Phellinus igniarius - Yenilemez': 70, 'Phellinus tremulae - Yenilemez': 71,
-    'Phlebia radiata - Yenilemez': 72, 'Pholiota aurivella - Yenilebilir': 73, 'Pholiota squarrosa - Yenilemez': 74, 'Physcia adscendens - Yenilemez': 75,
-    'Platismatia glauca - Yenilemez': 76, 'Pleurotus ostreatus - Yenilebilir': 77, 'Pleurotus pulmonarius - Yenilebilir': 78, 'Pseudevernia furfuracea - Yenilemez': 79,
-    'Rhytisma acerinum - Yenilemez': 80, 'Sarcomyxa serotina - Yenilebilir': 81, 'Sarcoscypha austriaca - Yenilemez': 82, 'Sarcosoma globosum - Yenilemez': 83,
-    'Schizophyllum commune - Yenilebilir': 84, 'Stereum hirsutum - Yenilemez': 85, 'Stropharia aeruginosa - Yenilemez': 86, 'Suillus granulatus - Yenilebilir': 87,
-    'Suillus grevillei - Yenilebilir': 88, 'Suillus luteus - Yenilebilir': 89, 'Trametes hirsuta - Yenilemez': 90, 'Trametes ochracea - Yenilemez': 91,
-    'Trametes versicolor - Yenilebilir': 92, 'Tremella mesenterica - Yenilebilir': 93, 'Trichaptum biforme - Yenilemez': 94, 'Tricholomopsis rutilans - Yenilemez': 95,
-    'Urnula craterium - Yenilemez': 96, 'Verpa bohemica - Yenilebilir': 97, 'Vulpicida pinastri - Yenilemez': 98, 'Xanthoria parietina - Yenilemez': 99
+    'Amanita muscaria - Yenilemez': 0,
+    'Amanita pantherina - Yenilemez': 1,
+    'Armillaria borealis - Yenilebilir': 2,
+    'Cantharellus cibarius - Yenilebilir':3,
+    'Cerioporus squamosus - Yenilebilir': 4,
+    'Clitocybe nebularis - Yenilemez': 5,
+    'Coltricia perennis - Yenilemez': 6,
+    'Gyromitra infula - Yenilemez': 7,
+    'Hericium coralloides - Yenilebilir': 8,
+    'Lactarius torminosus - Yenilemez':     9,
+    'Leccinum scabrum - Yenilebilir':  10,
+    'Lepista nuda - Yenilebilir':  11,
+    'Macrolepiota procera - Yenilebilir':  12,
+    'Mutinus ravenelii - Yenilemez':  13,
+    'Paxillus involutus - Yenilemez':  14,
+    'Phallus impudicus - Yenilemez':  15,
+    'Phlebia radiata - Yenilemez':  16,
+    'Pholiota aurivella - Yenilebilir':  17,
+    'Sarcosoma globosum - Yenilemez':  18,
+    'Stereum hirsutum - Yenilemez':  19,
+    'Trametes versicolor - Yenilebilir':  20,
+    'Xanthoria parietina - Yenilemez':  21,
 }
+
 
 # OpenAI API key configuration
 openai.api_key = 'sk-proj-Wf5pvmDri1gKKJnVbKDZT3BlbkFJbqTsLh9IEruCuZQRkcMY'
